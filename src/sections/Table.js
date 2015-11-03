90a472542ec576d84e4943a64e985f027255f06b
Kevin Smith <ksmithbaylor@gmail.com>
2015-11-02 19:02:41 -0500

    Style ValidUntil cells with dates

--- src/sections/Table.js
+++ src/sections/Table.js
@@ -2,6 +2,12 @@ import React from 'react';
 
 import COLUMN_NAMES from '../data/columnNames';
 
+const boldRed = {
+  color: 'red',
+  fontWeight: 'bold',
+  backgroundColor: 'pink'
+};
+
 export default class Table extends React.Component {
   render() {
     if (!this.props.license) {
@@ -19,7 +25,7 @@ export default class Table extends React.Component {
           {this.props.license.map((row, i) => (
             <tr key={i}>
               {COLUMN_NAMES.map((column, i) => (
-                  <td style={{ color: (column === 'Valid Until' && row[column] !== '0') ? 'red' : 'black' }}
+                  <td style={(column === 'Valid Until' && row[column] !== '0') ? boldRed : {}}
                       key={i}>{row[column]}</td>
               ))}
             </tr>
