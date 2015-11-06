7e88975517ffbb7341a9e679ce71b3d3919ac7d2
Kevin Smith <ksmithbaylor@gmail.com>
2015-11-05 23:32:51 -0500

    Make colors more consistent and add NAME color grouping

--- src/sections/Table.js
+++ src/sections/Table.js
@@ -41,16 +41,20 @@ function cellContents(row, column) {
 function cellStyle(row, column) {
   return {
     fontWeight: (column === VALID) ? 'bold' : 'inherit',
-    textAlign: (column === CODE || column === NAME) ? 'left' : 'center'
+    textAlign: (column === CODE || column === NAME) ? 'left' : 'center',
+    backgroundColor: (column === NAME) ? (
+      (row[NAME].startsWith(' ')) ? '#BBDEFB' : '#64B5F6'
+    ) : undefined,
+    color: (column === NAME) ? '#000000' : undefined
   };
 }
 
 function rowStyle(row) {
   return isExpired(row) ? {
-    color: 'red',
-    backgroundColor: 'pink'
+    color: '#B71C1C',
+    backgroundColor: '#FFCDD2'
   } : hasExpiry(row) ? {
-    color: 'darkgreen',
-    backgroundColor: 'lightgreen'
+    color: '#1B5E20',
+    backgroundColor: '#C8E6C9'
   } : {};
 }
