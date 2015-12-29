3cff6c026ac19e0bb0fd90c0fbc2488f718af45a
Kevin Smith <ksmithbaylor@gmail.com>
2015-12-29 12:42:06 -0500

    Revert "Move output to main folder out of dist"
    This reverts commit ccf08542639d68b4849878c370fa3266f4783cca.

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -7,9 +7,9 @@ module.exports = {
     './src/index'
   ],
   output: {
-    path: __dirname,
+    path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/'
+    publicPath: '/dist/'
   },
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
