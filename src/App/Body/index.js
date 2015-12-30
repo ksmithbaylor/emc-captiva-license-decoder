899d50e312c0946c79eff10c1a10bebd3b323369
Kevin Smith <ksmithbaylor@gmail.com>
2015-12-29 20:04:00 -0500

    Clean up Body and split out ErrorDialog

--- src/App/Body/index.js
+++ src/App/Body/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 
-import { pageWidth } from 'data/layout';
-
-import Dialog from 'material-ui/lib/dialog';
-import FlatButton from 'material-ui/lib/flat-button';
-
 import InputOptions from './InputOptions';
 import Display from './Display';
+import ErrorDialog from './ErrorDialog';
+
+import { pageWidth } from 'data/layout';
 
 export default class Body extends React.Component {
   state = {
@@ -19,48 +17,26 @@ export default class Body extends React.Component {
   render() {
     const { modules, serverID, showResults, errorModalOpen } = this.state;
 
-    const viewMarkup = showResults ? (
+    const mainViewMarkup = showResults ? (
       <Display
         modules={modules}
         serverID={serverID}
         backToStart={this.backToStart}
       />
     ) : (
-      <InputOptions
-        newResults={this.handleNewResults}
-      />
-    );
-
-    const okButtonMarkup = (
-      <FlatButton
-        label="OK"
-        primary={true}
-        onTouchTap={this.closeErrorModal}
-      />
+      <InputOptions newResults={this.handleNewResults} />
     );
 
     return (
-      <div
-        style={{
-          width: pageWidth,
-          marginTop: '8rem',
-          marginLeft: 'auto',
-          marginRight: 'auto'
-        }}
-      >
-        {viewMarkup}
-        <Dialog
-          title="Error"
-          open={errorModalOpen}
-          modal={false}
-          actions={ [okButtonMarkup] }
-          onRequestClose={this.closeErrorModal}
-        >
-          The input was invalid.
-        </Dialog>
+      <div style={containerStyle}>
+        {mainViewMarkup}
+        <ErrorDialog open={errorModalOpen} closeMe={this.closeErrorModal} />
       </div>
-    )
+    );
   }
+
+  backToStart = () => this.setState({ showResults: false })
+  closeErrorModal = () => this.setState({ errorModalOpen: false })
 
   handleNewResults = ({ modules, serverID }) => {
     if (!modules || !serverID || modules.length === 0 || modules.error) {
@@ -69,12 +45,11 @@ export default class Body extends React.Component {
       this.setState({ modules, serverID, showResults: true });
     }
   }
-
-  backToStart = () => {
-    this.setState({ showResults: false });
-  }
-
-  closeErrorModal = () => {
-    this.setState({ errorModalOpen: false });
-  }
 }
+
+const containerStyle = {
+  width: pageWidth,
+  marginTop: '8rem',
+  marginLeft: 'auto',
+  marginRight: 'auto'
+};
