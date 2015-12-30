715ef3af850c4616fb7d78bdb98f372a4d65a0df
Kevin Smith <ksmithbaylor@gmail.com>
2015-12-30 13:39:48 -0500

    Make FileInput stateless

--- src/App/Body/InputOptions/FileInput/index.js
+++ src/App/Body/InputOptions/FileInput/index.js
@@ -2,22 +2,18 @@ import React from 'react';
 import RaisedButton from 'material-ui/lib/raised-button';
 import FileHandler from './FileHandler';
 
-export default class FileInput extends React.Component {
-  render() {
-    const { sectionStyle, buttonStyle, requestResults } = this.props;
-
-    return (
-      <div style={sectionStyle}>
-        <RaisedButton
-          label="OPEN"
-          primary={true}
-          linkButton={true}
-          style={buttonStyle}
-        >
-          <FileHandler requestResults={requestResults} />
-        </RaisedButton>
-        Open a license file from your computer
-      </div>
-    );
-  }
+export default function FileInput({ sectionStyle, buttonStyle, requestResults }) {
+  return (
+    <div style={sectionStyle}>
+      <RaisedButton
+        label="OPEN"
+        primary={true}
+        linkButton={true}
+        style={buttonStyle}
+      >
+        <FileHandler requestResults={requestResults} />
+      </RaisedButton>
+      Open a license file from your computer
+    </div>
+  );
 }
