db5a7fac378f2ee6ffc98e734f8c7719e1cdf2e8
Kevin Smith <ksmithbaylor@gmail.com>
2016-07-22 22:26:43 -0400

    Don't consider DPMANAGER modules indicative of enterprise license

--- src/App/Body/Display/Summary/SummaryBody/summaryFunctions/isEnterprise.js
+++ src/App/Body/Display/Summary/SummaryBody/summaryFunctions/isEnterprise.js
@@ -5,7 +5,7 @@ export default function isEnterprise(modules) {
   const serverModule = modules.find(withName('SERVER'));
 
   const hasFeatures = /[CDEFSW]/.test(serverModule[FEATURES]);
-  const hasModules = ['DPMANAGER', 'WSINPUT', 'WSOUTPUT', 'ECOPY'].some(name => (
+  const hasModules = ['WSINPUT', 'WSOUTPUT', 'ECOPY'].some(name => (
     modules.some(withName(name))
   ));
 
