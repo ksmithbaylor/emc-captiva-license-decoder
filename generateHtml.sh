c6552fc2dfd5c208a78f27c8d99658f3192d4fc4
Kevin Smith <ksmithbaylor@gmail.com>
2016-12-27 10:47:50 -0600

    add Google analytics

--- generateHtml.sh
+++ generateHtml.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
 rm -f index.html
-cat dev.html | grep -v '</body>' | grep -v '</html>' | grep -v '<script' >> index.html
+cat dev.html | grep -v '</body>' | grep -v '</html>' | grep -v 'dist/bundle' >> index.html
 echo '<script>' >> index.html
 cat dist/bundle.js >> index.html
 echo '</script></body></html>' >> index.html
