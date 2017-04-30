ee751c1ee3af15a47b057a5c3c1922fc60cfd9ca
Kevin Smith <ksmithbaylor@gmail.com>
2017-04-29 21:44:13 -0400

    Update deploy script for yarn and not auto update version

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,12 @@
 #!/usr/bin/env zsh
 
-if [[ $# -lt 1 ]]; then
-  echo "Usage: ./deploy.sh {major,minor,patch}"
-  exit 1
-fi
-
-# Bump version
-npm version "$1"
-
 # Push to master
 git push
 
 # Commit changes to gh-pages branch, stop if anything fails
 git checkout gh-pages && \
 git merge master && \
-npm run build && \
+yarn run build && \
 git commit -am 'Deploy latest version'
 
 # Push whatever happened to gh-pages and switch back to master
